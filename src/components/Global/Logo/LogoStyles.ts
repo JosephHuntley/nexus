7976f2f89944301b0639970f4a6c1a8031d1c209
Joseph Huntley <Joseph.Huntley@outlook.com>
2023-04-10 12:16:08 -0400

    Increased Logo size on large devices

--- src/components/Global/Logo/LogoStyles.ts
+++ src/components/Global/Logo/LogoStyles.ts
@@ -10,7 +10,14 @@ export const LogoImg = styled.div`
 	width: 44px;
 	height: 38px;
 	margin-right: 16px;
+
+	@media ${props => props.theme.breakpoints.xl} {
+		border: 12px solid #fff;
+		width: 5.5rem;
+		height: 5.5rem;
+	}
 `;
+
 export const LogoText = styled.h1`
 	background: linear-gradient(
 		90deg,
@@ -20,6 +27,10 @@ export const LogoText = styled.h1`
 	width: max-content;
 	-webkit-background-clip: text;
 	-webkit-text-fill-color: transparent;
-	font-size: 26px;
+	font-size: 2.6rem;
 	font-weight: bold;
+
+	@media ${props => props.theme.breakpoints.xl} {
+		font-size: 4rem;
+	}
 `;
