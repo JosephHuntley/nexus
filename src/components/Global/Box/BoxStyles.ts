13510a1c9e2a182e39ec9683fa2694a89809790f
Joseph Huntley <Joseph.Huntley@outlook.com>
2023-04-10 17:30:52 -0400

    Removed Border, Changed Box-Shadow

--- src/components/Global/Box/BoxStyles.ts
+++ src/components/Global/Box/BoxStyles.ts
@@ -2,24 +2,21 @@ import styled from 'styled-components';
 import { BoxProps } from './Box';
 
 export const Container = styled.div<BoxProps>`
-	border: 1px solid ${(props) => props.theme.colors.text_secondary};
 	background: ${(props) => props.theme.colors.bg_white};
-	border-radius: ${(props) => props.radius};
+	border-radius: ${(props) => props.radius || '16px'};
 	overflow: auto;
 	white-space: nowrap;
 	width: ${(props) => props.width};
 	height: ${(props) => props.height};
 	/* First checks if there should be a box shadow. Then checks if dark mode is enabled */
-	/* TODO: Delete box-shadow or keep it  */
-	/* box-shadow: ${(props) =>
+	
+	box-shadow: ${(props) =>
 		props.isShadow
 			? props.theme.colors.bg_white === '#fff'
-				? '0px 4px 4px rgba(0, 0, 0, 0.25)'
+				? '0px 8px 8px rgba(0, 0, 0, .2)'
 				: 'none'
-			: 'none'}; */
-	/* &:hover {
-		box-shadow: none;
-	} */
+			: 'none'};
+			
 
 	@media ${(props) => props.theme.breakpoints.tablet} {
 		width: ${(props) => props.md_width};
