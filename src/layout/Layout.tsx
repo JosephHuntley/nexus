453b7905d9cab262a2dac2ee4869cff6892e08b0
Joseph Huntley <Joseph.Huntley@outlook.com>
2023-04-28 18:10:19 -0400

    Prevented pre-display of page if not logged in

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,49 +1,51 @@
-import React, { useEffect, ReactNode, useState } from 'react';
-import { useRouter } from 'next/router';
-import Sidebar from './Sidebar/Sidebar';
-import Header from './Header/Header';
-import { Main, LoaderContainer } from './LayoutStyles';
-import useWindowSize from '@/customHooks/useWindowSize';
-import MobileMenu from './MobileMenu/MobileMenu';
-import Footer from './Footer/Footer';
-import { RingLoader as Loader } from 'react-spinners';
+import React, { useEffect, ReactNode, useState } from "react"
+import { useRouter } from "next/router"
+import Sidebar from "./Sidebar/Sidebar"
+import Header from "./Header/Header"
+import { Main } from "./LayoutStyles"
+import useWindowSize from "@/customHooks/useWindowSize"
+import MobileMenu from "./MobileMenu/MobileMenu"
+import Footer from "./Footer/Footer"
 
 type props = {
-	children: ReactNode;
-};
-
-function Layout({ children }: props) {
-	const [isMenu, setIsMenu] = useState(true);
-
-	const router = useRouter();
-	const windowSize = useWindowSize();
-
-	useEffect(() => {
-		setIsMenu(windowSize.width <= 1124);
-	}, [windowSize]);
-
-	// TODO: Handle redirect if user not logged in.
-	useEffect(() => {
-		if (window.localStorage.getItem('isLoggedIn') !== 'true') {
-			router.push('/');
-		}
-	}, []);
-
-	return (
-		<>
-			{/* If the device is below the width it display a hamburger menu instead of  header and sidebar*/}
-			{isMenu ? (
-				<MobileMenu />
-			) : (
-				<>
-					<Header />
-					<Sidebar />
-				</>
-			)}
-			<Main>{children}</Main>
-			<Footer />
-		</>
-	);
+  children: ReactNode
 }
 
-export default Layout;
+function Layout({ children }: props) {
+  const [isMenu, setIsMenu] = useState(true)
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+  const router = useRouter()
+  const windowSize = useWindowSize()
+
+  useEffect(() => {
+    setIsMenu(windowSize.width <= 1124)
+  }, [windowSize])
+
+  // TODO: Handle redirect if user not logged in.
+  useEffect(() => {
+    if (window.localStorage.getItem("isLoggedIn") !== "true") {
+      router.push("/")
+    } else {
+      setIsLoggedIn(true)
+    }
+  }, [])
+
+  return isLoggedIn ? (
+    <>
+      {/* If the device is below the width it display a hamburger menu instead of  header and sidebar*/}
+      {isMenu ? (
+        <MobileMenu />
+      ) : (
+        <>
+          <Header />
+          <Sidebar />
+        </>
+      )}
+      <Main>{children}</Main>
+      <Footer />
+    </>
+  ) : null
+}
+
+export default Layout
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,54 +1,48 @@
 /* eslint-disable react-hooks/rules-of-hooks */
-import Head from 'next/head';
-import { Main, LeftSide, RightSide } from '../styles/pages/indexStyles';
+import Head from "next/head"
+import { Main, LeftSide, RightSide } from "../styles/pages/indexStyles"
 
 // Right Side
-import LoginBox from '../components/Authentication/LoginBox/LoginBox';
+import LoginBox from "../components/Authentication/LoginBox/LoginBox"
 // Left Side
-import TextMessage from '@/components/Authentication/TextMessage/TextMessage';
-import Alert from '@/components/Global/Alert/Alert';
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
+import TextMessage from "@/components/Authentication/TextMessage/TextMessage"
+import Alert from "@/components/Global/Alert/Alert"
+import { useEffect, useState } from "react"
+import { useRouter } from "next/router"
 
 export default function Login() {
-	const route = useRouter();
+  const [isNotLoggedIn, setIsNotLoggedIn] = useState(false)
+  const route = useRouter()
 
-	useEffect(() => {
-		if (window.localStorage.getItem('isLoggedIn') === 'true') {
-			route.push('/dashboard');
-		}
-	}, []);
+  useEffect(() => {
+    if (window.localStorage.getItem("isLoggedIn") === "true") {
+      route.push("/dashboard")
+    } else {
+      setIsNotLoggedIn(true)
+    }
+  }, [])
 
-	return (
-		<>
-			{/* Adds things to the head tag of the page */}
-			<Head>
-				<title>Login</title>
-				<meta
-					name='description'
-					content='HRIS Nexus Authentication Page'
-				/>
-				<meta
-					name='viewport'
-					content='width=device-width, initial-scale=1'
-				/>
-				<link
-					rel='icon'
-					href='/favicon.ico'
-				/>
-			</Head>
-			<Main>
-				<Alert open={true}>
-					This site is still under development. To login, hit the login button
-					without credentials.
-				</Alert>
-				<LeftSide>
-					<TextMessage />
-				</LeftSide>
-				<RightSide>
-					<LoginBox />
-				</RightSide>
-			</Main>
-		</>
-	);
+  return isNotLoggedIn ? (
+    <>
+      {/* Adds things to the head tag of the page */}
+      <Head>
+        <title>Login</title>
+        <meta name="description" content="HRIS Nexus Authentication Page" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Main>
+        <Alert open={true}>
+          This site is still under development. To login, hit the login button
+          without credentials.
+        </Alert>
+        <LeftSide>
+          <TextMessage />
+        </LeftSide>
+        <RightSide>
+          <LoginBox />
+        </RightSide>
+      </Main>
+    </>
+  ) : null
 }
